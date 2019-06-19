ac1bd6472148195dcb827993fab92e338f762703
rhn014 <Roland.Hyden@RegionHalland.se>
2019-06-19 10:12:29 +0200

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,87 @@
-# region-halland-acf-theme-links
-Skapa tema länkar
+# Skapa en "tema länk"
+
+## Hur man använder Region Hallands plugin "region-halland-acf-theme-links"
+
+Nedan följer instruktioner hur du kan använda pluginet "region-halland-acf-theme-links".
+
+
+## Användningsområde
+
+Denna plugin skapar funktionalitet för en "tema länkar", dvs:
+
+```sh
+A) Lägger till fyra fält i databasen, dvs 2 fält för respektive länk. Dels rubrik, dels länk
+B) Skapar ett formulär under "Temainställningar" i Wp-admin där man kan editera texterna
+C) En funktion för att hämta ut respektive text som en array
+```
+
+OBS! Denna plugin förutsätter att du har installerat och aktiverat Advanced Custom Fields Pro (https://www.advancedcustomfields.com/pro/)
+
+
+## Installation och aktivering
+
+```sh
+A) Hämta pluginen via Git eller läs in det med Composer
+B) Installera Region Hallands plugin i Wordpress plugin folder
+C) Aktivera pluginet inifrån Wordpress admin
+```
+
+
+## Hämta hem pluginet via Git
+
+```sh
+git clone https://github.com/RegionHalland/region-halland-acf-theme-links.git
+```
+
+
+## Läs in pluginen via composer
+
+Dessa två delar behöver du lägga in i din composer-fil
+
+Repositories = var pluginen är lagrad, i detta fall på github
+
+```sh
+"repositories": [
+  {
+    "type": "vcs",
+    "url": "https://github.com/RegionHalland/region-halland-acf-theme-links.git"
+  },
+],
+```
+Require = anger vilken version av pluginen du vill använda, i detta fall version 1.0.0
+
+OBS! Justera så att du hämtar aktuell version.
+
+```sh
+"require": {
+  "regionhalland/region-halland-acf-theme-links": "1.0.0"
+},
+```
+
+
+## Visa "tema länkar" på en sida via "Blade"
+
+```sh
+@if(function_exists('get_region_halland_theme_links'))
+  @php($myData = get_region_halland_theme_links())
+    <a href="{!! $myData['link_1'] !!}">{!! $myData['text_1'] !!}</a><br>
+    <a href="{!! $myData['link_2'] !!}">{!! $myData['text_2'] !!}</a><br>
+@endif
+```
+
+
+## Exempel på hur arrayen kan se ut
+
+```sh
+array (size=4)
+  'text_1' => string 'Min sida' (length=8)
+  'link_1' => string 'http://minsida.se' (length=17)
+  'text_2' => string 'Min andra sida' (length=14)
+  'link_2' => string 'http://www.minandrasida.se' (length=26)
+```
+
+
+## Versionhistorik
+
+### 1.0.0
+- Första version
---
+++ region_halland_acf_theme_links.php
@@ -0,0 +1,191 @@
+<?php
+
+	/**
+	 * @package Region Halland ACF Theme Links
+	 */
+	/*
+	Plugin Name: Region Halland ACF Theme Links
+	Description: ACF-fält för tema länkar
+	Version: 1.0.0
+	Author: Roland Hydén
+	License: MIT
+	Text Domain: regionhalland
+	*/
+
+	// Anropa funktion om ACF är installerad & aktiverad
+	add_action('acf/init', 'my_acf_add_theme_links_field_groups');
+
+	// Lägg till ett formulär
+	function my_acf_add_theme_links_field_groups() {
+
+		// Om funktionen existerar
+		if (function_exists('acf_add_local_field_group')):
+
+			// Lägg till formulär via acf
+			acf_add_local_field_group(array(
+				'key' => 'group_1000180',
+				'title' => 'Hantera länkar för jobb',
+				'fields' => array(
+					array(
+						'key' => 'field_1000181',
+						'label' => 'Länkar',
+						'name' => '',
+						'type' => 'tab',
+						'instructions' => '',
+						'required' => 0,
+						'conditional_logic' => 0,
+						'wrapper' => array(
+							'width' => '',
+							'class' => '',
+							'id' => '',
+						),
+						'placement' => 'top',
+						'endpoint' => 0,
+					),
+					array(
+						'key' => 'field_1000182',
+						'label' => '',
+						'name' => 'name_1000183',
+						'type' => 'group',
+						'instructions' => '',
+						'required' => 0,
+						'conditional_logic' => 0,
+						'wrapper' => array(
+							'width' => '',
+							'class' => '',
+							'id' => '',
+						),
+						'layout' => 'block',
+						'sub_fields' => array(
+							array(
+								'key' => 'field_1000184',
+								'label' => '1) Vad söker ni',
+								'name' => 'name_1000185',
+								'type' => 'text',
+								'instructions' => 'Ange vad ni söker',
+								'required' => 0,
+								'conditional_logic' => 0,
+								'wrapper' => array(
+									'width' => '',
+									'class' => '',
+									'id' => '',
+								),
+								'default_value' => '',
+								'placeholder' => '',
+								'maxlength' => '',
+								'rows' => 3,
+								'new_lines' => '',
+							),
+							array(
+								'key' => 'field_1000186',
+								'label' => '1) Länk till annons',
+								'name' => 'name_1000187',
+								'type' => 'text',
+								'instructions' => 'Skriv in fullständig länk till annons.',
+								'required' => 0,
+								'conditional_logic' => 0,
+								'wrapper' => array(
+									'width' => '',
+									'class' => '',
+									'id' => '',
+								),
+								'default_value' => '',
+								'placeholder' => '',
+								'maxlength' => '',
+								'rows' => 3,
+								'new_lines' => '',
+							),
+							array(
+								'key' => 'field_1000188',
+								'label' => '2) Vad söker ni',
+								'name' => 'name_1000189',
+								'type' => 'text',
+								'instructions' => 'Ange vad ni söker',
+								'required' => 0,
+								'conditional_logic' => 0,
+								'wrapper' => array(
+									'width' => '',
+									'class' => '',
+									'id' => '',
+								),
+								'default_value' => '',
+								'placeholder' => '',
+								'maxlength' => '',
+								'rows' => 3,
+								'new_lines' => '',
+							),
+							array(
+								'key' => 'field_1000190',
+								'label' => '2) Länk till annons',
+								'name' => 'name_1000191',
+								'type' => 'text',
+								'instructions' => 'Skriv in fullständig länk till annons.',
+								'required' => 0,
+								'conditional_logic' => 0,
+								'wrapper' => array(
+									'width' => '',
+									'class' => '',
+									'id' => '',
+								),
+								'default_value' => '',
+								'placeholder' => '',
+								'prepend' => '',
+								'append' => '',
+								'maxlength' => '',
+							),
+						),
+					)
+				),
+				'location' => array(
+					array(
+						array(
+							'param' => 'options_page',
+							'operator' => '==',
+							'value' => 'acf-options-temainstallningar',
+						),
+					),
+				),
+				'menu_order' => 0,
+				'position' => 'normal',
+				'style' => 'default',
+				'label_placement' => 'top',
+				'instruction_placement' => 'label',
+				'hide_on_screen' => '',
+				'active' => 1,
+				'description' => '',
+			));
+
+		endif;
+
+	}
+
+	// Hämta ACF-texter för cokkie-notice
+	function get_region_halland_theme_links()
+	{
+		// Hämta respektive värde från databasen
+		$links['text_1'] = get_field('name_1000183_name_1000185', 'options');
+		$links['link_1'] = get_field('name_1000183_name_1000187', 'options');
+		$links['text_2'] = get_field('name_1000183_name_1000189', 'options');
+		$links['link_2'] = get_field('name_1000183_name_1000191', 'options');
+		
+		// Returnera värdena som en array
+		return $links;
+	}
+
+	// Metod som anropas när pluginen aktiveras
+	function region_halland_acf_theme_links_activate() {
+		// Ingenting just nu...
+	}
+
+	// Metod som anropas när pluginen av-aktiveras
+	function region_halland_acf_theme_links_deactivate() {
+		// Ingenting just nu...
+	}
+
+	// Aktivera pluginen och anropa metod
+	register_activation_hook( __FILE__, 'region_halland_acf_theme_links_activate');
+
+	// Av-aktivera pluginen och anropa metod
+	register_deactivation_hook( __FILE__, 'region_halland_acf_theme_links_deactivate');
+
+?>
